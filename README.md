daaa41c03f63284129c160d69fd06a5a514a83aa
rasan000 <rasan000@gmail.com>
2024-06-11 15:27:24 +0900

    クリップボード共有を追加

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # overview
 neovimの自分用設定ファイル
+
+# クリップボードを有効にする
+wsl(ubuntuの場合) sudo apt install xclip
--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
 if not vim.g.vscode then
     require("keymap")
     require("plugin")
+else
+    vim.opt.clipboard="unnamed,unnamedplus"
 end
