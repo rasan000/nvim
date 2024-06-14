1c6ef925e758c6ef5b64b1b5825d656a6ca322d0
rasan000 <rasan000@gmail.com>
2024-06-14 09:12:49 +0900

    set linenumber apper absolute

--- init.lua
+++ init.lua
@@ -2,5 +2,5 @@ if not vim.g.vscode then
     require("keymap")
     require("plugin")
 else
-    vim.opt.clipboard="unnamed,unnamedplus"
+    -- vim.opt.clipboard="unnamed,unnamedplus"
 end
--- lua/keymap.lua
+++ lua/keymap.lua
@@ -3,7 +3,7 @@
 
 -- 行番号と相対行番号を表示する
 vim.opt.number = true
-vim.opt.relativenumber = true
+-- vim.opt.relativenumber = true
 
 -- タブを4文字に設定する
 vim.opt.tabstop = 4
