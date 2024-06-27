f6b70ff2d7d46a0809eee3e8f1bfad36aafd400c
rasan000 <rasan000@gmail.com>
2024-06-27 22:14:44 +0900

    which key追加

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -265,6 +265,21 @@ require('lazy').setup({
             -- See Configuration section for rest
         },
         vim.api.nvim_set_keymap('n', '<leader>p', '<Cmd>CopilotChatToggle<CR>', {})
+    },
+
+    --which-key
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
     }
 })
 
