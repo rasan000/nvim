ce21f649fca781c4f711ba53251f486183b40bd1
rasan000 <rasan000@gmail.com>
2024-07-06 18:58:03 +0900

    toggleTerm調整

--- lua/plugins/basic.lua
+++ lua/plugins/basic.lua
@@ -27,13 +27,22 @@ return {
         'akinsho/toggleterm.nvim',
         config = function()
             require('toggleterm').setup({
-                size = 20,
+                size = 25,
                 winblend = 15,
-                open_mapping = [[<c-t>]],
-                direction = 'float',
+                open_mapping = [[<c-\>]],
+                direction = 'horizontal',
                 border = 'double', -- または 'single', 'shadow', 'curved'
             })
             vim.api.nvim_create_user_command('Term', function() require('toggleterm').toggle() end, {})
+            local opts = {buffer = 0}
+            vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+            vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+            vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+            vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+            vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+            vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+            vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+
         end
     },
 
@@ -91,6 +100,6 @@ return {
             vim.o.timeout = true
             vim.o.timeoutlen = 300
         end,
-        opts = { }
+        opts = {}
     }
 }
--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -23,6 +23,8 @@ return {
                 [[coc#pum#visible() ? coc#pum#confirm() : "\<c-g>u\<cr>\<c-r>=coc#on_enter()\<cr>"]], opts)
             vim.keymap.set("i", "<C-n>", 'coc#pum#visible() ? coc#pum#next(0) : "<C-n>"', opts)
             vim.keymap.set("i", "<C-p>", [[coc#pum#visible() ? coc#pum#prev(0) : "<C-p>"]], opts)
+            vim.keymap.set("i", "<Tab>", 'coc#pum#visible() ? coc#pum#next(0) : "<Tab>"', opts)
+            vim.keymap.set("i", "<s-tab>", [[coc#pum#visible() ? coc#pum#prev(0) : "<c-h>"]], opts)
 
             vim.g.coc_global_extensions = { 'coc-html', 'coc-emmet', 'coc-json', 'coc-css', 'coc-yaml', 'coc-vimlsp',
                 'coc-tsserver', 'coc-eslint', 'coc-sumneko-lua', 'coc-docker', 'coc-snippets', 'coc-python', 'coc-pairs',
